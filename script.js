ea6712988ee4d72ed29f8f018ffcef10ebc81c43
Sardor <sardorabdurasulov2oo1@gmail.com>
2024-05-14 07:14:27 +0500

    js

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ const priceInput = document.querySelectorAll(".price-input input")
 
 let priceGap = 1000
 
-priceInput.forEach(input => {
-  input.addEventListener("input", e => {
-    let minVal = parseInt(priceInput[0].value),
-      maxVal = parseInt(priceInput[1].value)
+// priceInput.forEach(input => {
+//   input.addEventListener("input", e => {
+//     let minVal = parseInt(priceInput[0].value),
+//       maxVal = parseInt(priceInput[1].value)
 
-    if ((maxVal - minVal >= priceGap) && maxVal <= 10000) {
-      if (e.target.className === "input-min") {
-        rangeInput[0].value = minVal
-        progress.style.left = (minVal / rangeInput[0].max) * 100 + "%"
-      } else {
-        rangeInput[1].value = maxVal
-        progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%"
-      }
-    }
-  })
-})
+//     if ((maxVal - minVal >= priceGap) && maxVal <= 10000) {
+//       if (e.target.className === "input-min") {
+//         rangeInput[0].value = minVal
+//         progress.style.left = (minVal / rangeInput[0].max) * 100 + "%"
+//       } else {
+//         rangeInput[1].value = maxVal
+//         progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%"
+//       }
+//     }
+//   })
+// })
 
 rangeInput.forEach(input => {
   input.addEventListener("input", (e) => {
